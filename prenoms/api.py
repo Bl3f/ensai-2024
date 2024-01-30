6920413417ee08661cdcbeba163d0aa859a23e89
Christophe Blefari <christophe.blefari@gmail.com>
2024-01-30 15:59:45 +0100

    Add LLM route

--- prenoms/api.py
+++ prenoms/api.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
+import requests
 
 app = Flask(__name__)
 regr = joblib.load("model.v1.bin")
+LLM_API = os.getenv("LLM_API")
 
 
 def encode_prenom(prenom: str):
@@ -20,13 +23,36 @@ def encode_prenom(prenom: str):
 @app.route("/predict")
 def predict():
     name = request.args.get("name")
+    model = request.args.get("model")
 
     if name:
-        sexe = regr.predict([encode_prenom(name)])
-        response = {
-            "name": name,
-            "gender": 'M' if sexe[0] == 0 else 'F',
-        }
+        if model is None or model != 'llm':
+            sexe = regr.predict([encode_prenom(name)])
+            response = {
+                "name": name,
+                "gender": 'M' if sexe[0] == 0 else 'F',
+            }
+        else:
+            prompt = f"""
+                What's the gender of the name ${name}?
+                Instructions:
+                - Give me the response in only one character. 
+                - For instance 'M' for male and 'F' for female. 
+                - This is important you give me the anwser in only 1 character and not more.
+                - Expected answer "M" or "F"
+            """
+            url = f"{LLM_API}/api/generate"
+            llm_response = requests.post(url, json={"model": "mistral", "prompt": prompt, "stream": False})
+            sexe = llm_response.json()["response"].strip()
+
+            response = {
+                "name": name,
+                "gender": sexe[0],
+                "debug": llm_response.text,
+                "url": url,
+            }
+
+
         return jsonify(response)
     else:
         return jsonify({"error": "You need to pass name as GET parameter."})
